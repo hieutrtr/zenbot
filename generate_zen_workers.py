b1fa49144362ffb30be96dc3f44b8fd726fe8d0d
Hieu Tran <hieutt@adasiaholdings.com>
2017-12-25 22:37:16 +0700

    fix wrong param

--- generate_zen_workers.py
+++ generate_zen_workers.py
@@ -18,7 +18,7 @@ else:
             res = res.replace(repl,'')
         res = res.split(' ')
         if len(res) >= 3 and sys.argv[1] in res[2] and '-'+sys.argv[2] in res[2]:
-            workers['apps'].append({"name": res[2], "script": "./zenbot.sh", "args": "trade {} --paper --strategy {} --conf ./beta.js".format(res[2],res[3])})
+            workers['apps'].append({"name": res[2], "script": "./zenbot.sh", "args": "trade {} --paper --strategy {} --conf ./beta.js".format(res[2],sys.argv[3])})
 
 def chunks(l, n):
     res = []
