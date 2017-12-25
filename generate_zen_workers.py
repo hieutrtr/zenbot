50b8e3fa6e79b78d90d77b210506cbc09772376e
Hieu Tran <hieutt@adasiaholdings.com>
2017-12-25 22:23:47 +0700

    add strategy param

--- generate_zen_workers.py
+++ generate_zen_workers.py
@@ -18,7 +18,7 @@ else:
             res = res.replace(repl,'')
         res = res.split(' ')
         if len(res) >= 3 and sys.argv[1] in res[2] and '-'+sys.argv[2] in res[2]:
-            workers['apps'].append({"name": res[2], "script": "./zenbot.sh", "args": "trade {} --paper --strategy macd --conf ./beta.js".format(res[2])})
+            workers['apps'].append({"name": res[2], "script": "./zenbot.sh", "args": "trade {} --paper --strategy {} --conf ./beta.js".format(res[2],res[3])})
 
 def chunks(l, n):
     res = []
@@ -27,7 +27,7 @@ def chunks(l, n):
     return res
 
 print "Number of workers : " + str(len(workers["apps"]))
-workers["apps"] =  chunks(workers["apps"], int(sys.argv[3]))[int(sys.argv[4]) - 1]
+workers["apps"] =  chunks(workers["apps"], int(sys.argv[4]))[int(sys.argv[5]) - 1]
 with open("./workers/beta.json", "w") as workerFile:
     workerFile.write(json.dumps(workers,sort_keys=True,indent=4, separators=(',', ': ')))
     workerFile.close()
