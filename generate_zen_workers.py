c6d835b9b8777d13b31c290d176d3a3eb946d14f
Hieu Tran <hieutt@adasiaholdings.com>
2017-12-29 00:59:15 +0700

    update strategy ops config

--- generate_zen_workers.py
+++ generate_zen_workers.py
@@ -5,14 +5,18 @@ import subprocess
 import time
 import re
 
-# proc = subprocess.Popen(["./zenbot.sh","list-selectors", "|", "grep", "bittrex"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-# output, err = proc.communicate(b"input data that is passed to subprocess' stdin")
-# results = output.split('\n')
 coinmarketcap = Market()
 coins = coinmarketcap.ticker(limit=50)
-# print(coins)
-# for coin in coins:
-#     print(coin.get("symbol",None))
+
+strategy_conf = {
+    "trendline": {
+        "period": "5m"
+    },
+    "rsi": {
+        "period": "1h"
+    }
+}
+
 strategies = sys.argv[4].split(',') if sys.argv[4] else ["macd","trendline","neural","forex_analytics"]
 workers = {"apps": []}
 exchange = sys.argv[1]
@@ -21,9 +25,9 @@ for coin in coins[1:]:
     if coin.get("symbol", None):
         selector = '{}.{}-{}'.format(exchange,coin["symbol"],currency)
         for strategy in strategies:
-            workers['apps'].append({"name": selector+'.'+strategy, "script": "./zenbot.sh", "args": "trade {} --paper --strategy {} --conf ./beta.js".format(selector,strategy)})
-    # if len(res) >= 3 and sys.argv[1] in res[2] and '-'+sys.argv[2] in res[2]:
-    #     workers['apps'].append({"name": res[2], "script": "./zenbot.sh", "args": "trade {} --paper --strategy {} --conf ./beta.js".format(res[2],sys.argv[3])})
+            conf = strategy_conf.get("strategy",{})
+            ops = "--period {}".format(conf["period"]) if conf.get("period",None) != None else ""
+            workers['apps'].append({"name": selector+'.'+strategy, "script": "./zenbot.sh", "args": "trade {} --paper --strategy {} {} --conf ./beta.js".format(selector,strategy,ops)})
 
 def chunks(l, n):
     res = []
